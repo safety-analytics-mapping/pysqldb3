bc30ee9355117c637b5ccc3e789273e71aaf5d86
Seth Hostetter <51931648+shostetter@users.noreply.github.com>
2022-05-15 06:59:31 -0400

    fix for error type in table to csv test

--- pysqldb3/tests/test_table_to_csv.py
+++ pysqldb3/tests/test_table_to_csv.py
@@ -283,7 +283,7 @@ class Test_Table_to_CSV_PG:
                             schema=schema,
                             output_file=os.path.join(fldr, create_table_name + '.csv')
                             )
-        assert exc_info.type is FileNotFoundError
+        assert exc_info.type is OSError
 
         # clean up
         db.disconnect(quiet=True)
@@ -436,7 +436,7 @@ class Test_Table_to_CSV_MS:
                              schema=schema,
                              output_file=os.path.join(fldr, create_table_name + '.csv')
                              )
-        assert exc_info.type is FileNotFoundError
+        assert exc_info.type is OSError
 
         # clean up
         sql.disconnect(quiet=True)
