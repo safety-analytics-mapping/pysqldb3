a3190950d4a6b74fbdb80ffe2d22f01e283d342e
Eric Renart <blacksun9487@gmail.com>
2022-11-23 15:39:47 -0500

    standardized params in test_drop_table.py

--- pysqldb3/tests/test_drop_table.py
+++ pysqldb3/tests/test_drop_table.py
@@ -7,19 +7,19 @@ from .. import pysqldb3 as pysqldb
 config = configparser.ConfigParser()
 config.read(os.path.dirname(os.path.abspath(__file__)) + "\\db_config.cfg")
 
-db = pysqldb.DbConnect(db_type=config.get('PG_DB', 'TYPE'),
-                       server=config.get('PG_DB', 'SERVER'),
-                       db_name=config.get('PG_DB', 'DB_NAME'),
-                       user=config.get('PG_DB', 'DB_USER'),
-                       password=config.get('PG_DB', 'DB_PASSWORD'))
+pg_dbconn = pysqldb.DbConnect(db_type=config.get('PG_DB', 'TYPE'),
+                              host=config.get('PG_DB', 'SERVER'),
+                              db_name=config.get('PG_DB', 'DB_NAME'),
+                              username=config.get('PG_DB', 'DB_USER'),
+                              password=config.get('PG_DB', 'DB_PASSWORD'))
 
-sql = pysqldb.DbConnect(db_type=config.get('SQL_DB', 'TYPE'),
-                        server=config.get('SQL_DB', 'SERVER'),
-                        db_name=config.get('SQL_DB', 'DB_NAME'),
-                        user=config.get('SQL_DB', 'DB_USER'),
-                        password=config.get('SQL_DB', 'DB_PASSWORD'))
+ms_dbconn = pysqldb.DbConnect(db_type=config.get('SQL_DB', 'TYPE'),
+                              host=config.get('SQL_DB', 'SERVER'),
+                              db_name=config.get('SQL_DB', 'DB_NAME'),
+                              username=config.get('SQL_DB', 'DB_USER'),
+                              password=config.get('SQL_DB', 'DB_PASSWORD'))
 
-test_for_drop_table = 'test_for_drop_table_func_{}'.format(db.user)
+test_for_drop_table = 'test_for_drop_table_func_{username}'.format(username=pg_dbconn.username)
 
 
 class TestDropTablePG:
@@ -27,175 +27,167 @@ class TestDropTablePG:
         schema = 'working'
         query_string = """
         
-        DROP TABLE IF EXISTS working.{};
-        CREATE TABLE working.{} AS 
+        DROP TABLE IF EXISTS working.{table};
+        CREATE TABLE working.{table} AS 
     
         SELECT 'a' as col1, 'b' as col2;
     
-        """.format(test_for_drop_table, test_for_drop_table)
+        """.format(table=test_for_drop_table)
 
-        db.query(query_string)
-        db.drop_table(schema, test_for_drop_table)
-        assert not db.table_exists(test_for_drop_table, schema=schema)
+        pg_dbconn.query(query_string)
+        pg_dbconn.drop_table(schema_name=schema, table_name=test_for_drop_table)
+        assert not pg_dbconn.table_exists(table_name=test_for_drop_table, schema_name=schema)
 
     def test_remove_table_from_log(self):
         query_string = """
 
-        DROP TABLE IF EXISTS working.{};
-        CREATE TABLE working.{} AS 
-
+        DROP TABLE IF EXISTS working.{table};
+        CREATE TABLE working.{table} AS 
+        
         SELECT 'a' as col1, 'b' as col2;
+        
+        INSERT INTO working.__temp_log_table_{username}__ (tbl_id, table_owner,table_schema,table_name,created_on,expires) 
+            VALUES (0, '{username}', 'working','{username}','09/18/2020',null);
+        """.format(table=test_for_drop_table, username=pg_dbconn.username)
 
-        INSERT INTO working.__temp_log_table_{}__ (tbl_id, table_owner,table_schema,table_name,created_on,expires) VALUES (0, '{}', 'working','{}','09/18/2020',null);
-
-        """.format(test_for_drop_table, test_for_drop_table, db.user, db.user, test_for_drop_table)
-
-        db.query(query_string)
-        db.drop_table('working', test_for_drop_table)
+        pg_dbconn.query(query_string)
+        pg_dbconn.drop_table(schema_name='working', table_name=test_for_drop_table)
 
         log_check_query_string = """
         
             SELECT tbl_id, table_owner, table_schema, table_name, created_on, expires
-            FROM working.__temp_log_table_{}__
-            WHERE table_name = '{}'
+            FROM working.__temp_log_table_{username}__
+            WHERE table_name = '{table}'
             
-        """.format(db.user, test_for_drop_table)
+        """.format(username=pg_dbconn.username, table=test_for_drop_table)
 
-        assert len(db.dfquery(log_check_query_string)) == 0
+        assert len(pg_dbconn.dfquery(log_check_query_string)) == 0
 
     def test_table_not_in_log(self):
         query_string = """
-    
-            DROP TABLE IF EXISTS working.{};
-            CREATE TABLE working.{} AS 
-    
+            DROP TABLE IF EXISTS working.{table};
+            CREATE TABLE working.{table} AS 
+            
             SELECT 'a' as col1, 'b' as col2;
-            """.format(test_for_drop_table, test_for_drop_table)
+            """.format(table=test_for_drop_table)
 
-        db.query(query_string)
-        db.drop_table(schema_name='working', table_name=test_for_drop_table)
+        pg_dbconn.query(query_string)
+        pg_dbconn.drop_table(schema_name='working', table_name=test_for_drop_table)
 
         log_check_query_string = """
         
             SELECT tbl_id, table_owner, table_schema, table_name, created_on, expires
-            FROM working.__temp_log_table_{}__
-            WHERE table_name = '{}'
+            FROM working.__temp_log_table_{username}__
+            WHERE table_name = '{table}'
         
-        """.format(db.user, test_for_drop_table)
+        """.format(username=pg_dbconn.username, table=test_for_drop_table)
 
-        assert len(db.dfquery(log_check_query_string)) == 0
+        assert len(pg_dbconn.dfquery(log_check_query_string)) == 0
 
     def test_table_DNE_but_in_log(self):
         query_string = """
 
-        INSERT INTO working.__temp_log_table_{}__ (tbl_id, table_owner,table_schema,table_name,created_on,expires) VALUES (0, '{}', 'working','{}','09/18/2020',null);
+        INSERT INTO working.__temp_log_table_{username}__ (tbl_id, table_owner,table_schema,table_name,created_on,expires) 
+            VALUES (0, '{table}', 'working','{table}','09/18/2020',null);
+            """.format(username=pg_dbconn.username, table=test_for_drop_table)
 
-        """.format(db.user, db.user, test_for_drop_table)
-
-        db.query(query_string)
-        db.drop_table('working', table_name=test_for_drop_table)
+        pg_dbconn.query(query_string)
+        pg_dbconn.drop_table('working', table_name=test_for_drop_table)
 
         log_check_query_string = """
             SELECT tbl_id, table_owner, table_schema, table_name, created_on, expires
-            FROM working.__temp_log_table_{}__
-            WHERE table_name = '{}'
-        """.format(db.user, test_for_drop_table)
+            FROM working.__temp_log_table_{username}__
+            WHERE table_name = '{table}'
+        """.format(username=pg_dbconn.username, table=test_for_drop_table)
 
-        assert len(db.dfquery(log_check_query_string)) == 0
+        assert len(pg_dbconn.dfquery(log_check_query_string)) == 0
 
     def test_table_does_not_exist(self):
-        assert db.drop_table('working', test_for_drop_table) is None
+        assert pg_dbconn.drop_table('working', test_for_drop_table) is None
 
 
 class TestDropTableMS:
 
     def test_drop_table_basic(self):
-        table_name = 'sql_test_table_{}'.format(sql.user)  # from helper.py
+        table_name = 'sql_test_table_{username}'.format(username=ms_dbconn.username)  # from helper.py
 
         schema = 'dbo'
         query_string = """
-
-        IF OBJECT_ID('dbo.{0}', 'u') IS NOT NULL
-        DROP TABLE dbo.{0};
-
+        IF OBJECT_ID('dbo.{table}', 'u') IS NOT NULL
+        DROP TABLE dbo.{table};
+        
         SELECT 'a' as col1, 'b' as col2
-        INTO dbo.{0};
+        INTO dbo.{table};
+        """.format(table=test_for_drop_table)
 
-        """.format(test_for_drop_table)
+        ms_dbconn.query(query_string)
+        ms_dbconn.drop_table(schema_name=schema, table_name=test_for_drop_table)
 
-        sql.query(query_string)
-        sql.drop_table(schema, table_name=test_for_drop_table)
-
-        assert not sql.table_exists(test_for_drop_table, schema=schema)
+        assert not ms_dbconn.table_exists(test_for_drop_table, schema=schema)
 
     def test_remove_table_from_log(self):
-        table_name = 'sql_test_table_{}'.format(sql.user)  # from helper.py
+        table_name = 'sql_test_table_{table}'.format(table=ms_dbconn.username)  # from helper.py
         query_string = """
 
-        IF OBJECT_ID('dbo.{dt}', 'u') IS NOT NULL
-        DROP TABLE dbo.{dt};
+        IF OBJECT_ID('dbo.{table}', 'u') IS NOT NULL
+        DROP TABLE dbo.{table};
 
         SELECT 'a' as col1, 'b' as col2
-        INTO dbo.{dt};
+        INTO dbo.{table};
 
-        INSERT INTO [dbo].[__temp_log_table_{u}__] (table_owner,table_schema,table_name,created_on,expires) 
-        VALUES ('{u}', 'dbo','{dt}','09/18/2020',null);
+        INSERT INTO [dbo].[__temp_log_table_{username}__] (table_owner,table_schema,table_name,created_on,expires) 
+        VALUES ('{username}', 'dbo','{table_name}','09/18/2020',null);
 
-        """.format(dt=test_for_drop_table, u=sql.user)
+        """.format(table=test_for_drop_table, table_name=table_name, username=ms_dbconn.username)
 
-        sql.query(query_string)
-        sql.drop_table('dbo', table_name=test_for_drop_table)
+        ms_dbconn.query(query_string)
+        ms_dbconn.drop_table(schema_name='dbo', table_name=test_for_drop_table)
 
         log_check_query_string = """
             SELECT tbl_id, table_owner, table_schema, table_name, created_on, expires
-            FROM [dbo].[__temp_log_table_{}__]
-            WHERE table_name = '{}'
-        """.format(sql.user, test_for_drop_table)
+            FROM [dbo].[__temp_log_table_{username}__]
+            WHERE table_name = '{table}'
+        """.format(username=ms_dbconn.username, table=test_for_drop_table)
 
-        assert len(sql.dfquery(log_check_query_string)) == 0
+        assert len(ms_dbconn.dfquery(log_check_query_string)) == 0
 
     def test_table_not_in_log(self):
-        table_name = 'sql_test_table_{}'.format(sql.user)  # from helper.py
+        table_name = 'sql_test_table_{username}'.format(username=ms_dbconn.username)  # from helper.py
         query_string = """
 
-        IF OBJECT_ID('dbo.{t}', 'u') IS NOT NULL
-        DROP TABLE dbo.{t};
-
-
+        IF OBJECT_ID('dbo.{table}', 'u') IS NOT NULL
+        DROP TABLE dbo.{table};
         SELECT 'a' as col1, 'b' as col2
-        INTO dbo.{t};
+        INTO dbo.{table};
+        """.format(table=test_for_drop_table)
 
-        """.format(t=test_for_drop_table)
-
-        sql.query(query_string)
-        sql.drop_table('dbo', table_name=test_for_drop_table)
+        ms_dbconn.query(query_string)
+        ms_dbconn.drop_table(schema_name='dbo', table_name=test_for_drop_table)
 
         log_check_query_string = """
             SELECT tbl_id, table_owner, table_schema, table_name, created_on, expires
-            FROM [dbo].[__temp_log_table_{}__]
-            WHERE table_name = '{}'
-        """.format(sql.user, test_for_drop_table)
+            FROM [dbo].[__temp_log_table_{username}__]
+            WHERE table_name = '{table}'
+        """.format(username=ms_dbconn.username, table=test_for_drop_table)
 
-        assert len(sql.dfquery(log_check_query_string)) == 0
+        assert len(ms_dbconn.dfquery(log_check_query_string)) == 0
 
     def test_table_DNE_but_in_log(self):
         query_string = """
+        INSERT INTO [dbo].[__temp_log_table_{username}__] (table_owner,table_schema,table_name,created_on,expires) 
+        VALUES ('{username}', 'dbo','{table}','09/18/2020',null);
+        """.format(username=ms_dbconn.username, table=test_for_drop_table)
 
-        INSERT INTO [dbo].[__temp_log_table_{}__] (table_owner,table_schema,table_name,created_on,expires) 
-        VALUES ('{}', 'dbo','{}','09/18/2020',null);
-
-        """.format(sql.user, sql.user, test_for_drop_table)
-
-        sql.query(query_string)
-        sql.drop_table('dbo', table_name=test_for_drop_table)
+        ms_dbconn.query(query_string)
+        ms_dbconn.drop_table(schema_name='dbo', table_name=test_for_drop_table)
 
         log_check_query_string = """
             SELECT tbl_id, table_owner, table_schema, table_name, created_on, expires
-            FROM [dbo].[__temp_log_table_{}__]
-            WHERE table_name = '{}'
-        """.format(sql.user, test_for_drop_table)
+            FROM [dbo].[__temp_log_table_{username}__]
+            WHERE table_name = '{table}'
+        """.format(username=ms_dbconn.username, table=test_for_drop_table)
 
-        assert len(sql.dfquery(log_check_query_string)) == 0
+        assert len(ms_dbconn.dfquery(log_check_query_string)) == 0
 
     def test_table_does_not_exist(self):
-        assert sql.drop_table('dbo', table_name=test_for_drop_table) is None
+        assert ms_dbconn.drop_table('dbo', table_name=test_for_drop_table) is None
