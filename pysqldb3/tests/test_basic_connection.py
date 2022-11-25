8d7e4bba03de37dfcf798714654114c6765a7de1
Seth Hostetter <51931648+shostetter@users.noreply.github.com>
2022-11-25 16:27:00 -0500

    adds test just to check you can connect to the databases

---
+++ pysqldb3/tests/test_basic_connection.py
@@ -0,0 +1,21 @@
+import os
+import configparser
+
+from .. import pysqldb3 as pysqldb
+
+test_config = configparser.ConfigParser()
+test_config.read(os.path.dirname(os.path.abspath(__file__)) + "\\db_config.cfg")
+
+
+def test():
+    db = pysqldb.DbConnect(default=True, password=test_config.get('PG_DB', 'DB_PASSWORD'),
+                           user=test_config.get('PG_DB', 'DB_USER'))
+    assert True
+
+    sql = pysqldb.DbConnect(type=test_config.get('SQL_DB', 'TYPE'),
+                            server=test_config.get('SQL_DB', 'SERVER'),
+                            database=test_config.get('SQL_DB', 'DB_NAME'),
+                            user=test_config.get('SQL_DB', 'DB_USER'),
+                            password=test_config.get('SQL_DB', 'DB_PASSWORD'))
+
+    assert True
