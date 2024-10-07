988a2c3fa794c2aa05d7cdc57aa76fc0755fe4f5
Cindy Chen <cchen1@dot.nyc.gov>
2024-10-07 14:21:26 -0400

    Make changes to allow capitalization tests to pass

--- pysqldb3/tests/test_backup_table.py
+++ pysqldb3/tests/test_backup_table.py
@@ -73,8 +73,8 @@ class TestBackupTablesPg:
 
         # run backup
         db.drop_table(pg_schema, test_pg_from_backup)
-        schema_table_name = db.create_table_from_backup(test_back_file)
-        assert re.findall(r'[\-["\w"\]]*\.[-\["\w"\]]*', schema_table_name)
+
+        db.create_table_from_backup(test_back_file)
 
         # validate table exists
         assert db.table_exists(test_pg_from_backup, schema=pg_schema)
@@ -281,11 +281,11 @@ class TestBackupTablesPg:
         assert not os.path.isfile(test_back_file)
 
     def test_backup_tables_capitalized(self):
-        db.drop_table(table=test_pg_to_backup.capitalize(), schema=pg_schema)
+        db.drop_table(table=test_pg_to_backup, schema=pg_schema)
 
         # table schema
         db.query(f"""
-            CREATE TABLE {pg_schema}.{test_pg_to_backup.capitalize()} (
+            CREATE TABLE {pg_schema}.{test_pg_to_backup} (
                 id int,
                 column2 text,
                 column3 timestamp,
@@ -295,39 +295,39 @@ class TestBackupTablesPg:
         # populate table
         for i in range(10):
             db.query(f"""
-                INSERT INTO {pg_schema}.{test_pg_to_backup.capitalize()}
+                INSERT INTO {pg_schema}.{test_pg_to_backup}
                     (id, column2, column3, "1 test messy column")
                 values ({i}, '{i}', '{'2022-10-14 10:12:40-04'}', '{'test '*i}')
             """)
         # validate table created
-        assert db.table_exists(test_pg_to_backup.capitalize(), schema=pg_schema)
-        db.query(f"select count(*) cnt from {pg_schema}.{test_pg_to_backup.capitalize()}")
+        assert db.table_exists(test_pg_to_backup, schema=pg_schema)
+        db.query(f"select count(*) cnt from {pg_schema}.{test_pg_to_backup}")
         assert db.data[0][0] == 10
 
         if os.path.isfile(test_back_file):
             os.remove(test_back_file)
         assert not os.path.isfile(test_back_file)
 
-        db.backup_table(pg_schema, test_pg_to_backup.capitalize(), test_back_file, pg_schema, test_pg_from_backup.capitalize())
+        db.backup_table(pg_schema, test_pg_to_backup, test_back_file, pg_schema, test_pg_from_backup.capitalize())
         assert os.path.isfile(test_back_file)
 
         # run backup
         db.drop_table(pg_schema, test_pg_from_backup.capitalize())
-        schema_table_name = db.create_table_from_backup(test_back_file, case_sensitive = True)
+        db.create_table_from_backup(test_back_file)
 
         # validate table exists
-        assert db.table_exists(test_pg_from_backup.capitalize(), schema=pg_schema)
-        db.query(f"select count(*) cnt from {pg_schema}.{test_pg_from_backup.capitalize()}")
+        assert db.table_exists(test_pg_from_backup.capitalize(), schema=pg_schema, case_sensitive = True)
+        db.query(f'select count(*) cnt from {pg_schema}."{test_pg_from_backup.capitalize()}"')
         assert db.data[0][0] == 10
 
         # Validate schema matches
-        _to = db.get_table_columns(test_pg_to_backup.capitalize(), schema=pg_schema)
+        _to = db.get_table_columns(test_pg_to_backup, schema=pg_schema)
         _from = db.get_table_columns(test_pg_from_backup.capitalize(), schema=pg_schema)
         assert _to == _from
 
         # clean up
         db.cleanup_new_tables()
-        assert not db.table_exists(test_pg_to_backup.capitalize(), schema=pg_schema, case_sensitive = True)
+        assert not db.table_exists(test_pg_to_backup, schema=pg_schema, case_sensitive = True)
         assert not db.table_exists(test_pg_from_backup.capitalize(), schema=pg_schema, case_sensitive = True)
         if os.path.isfile(test_back_file):
             os.remove(test_back_file)
@@ -365,16 +365,16 @@ class TestBackupTablesMs:
             os.remove(test_back_file)
         assert not os.path.isfile(test_back_file)
 
-        sql.backup_table(ms_schema, test_sql_to_backup, test_back_file, ms_schema, test_sql_from_backup)
+        sql.backup_table(ms_schema, test_sql_to_backup, test_back_file, ms_schema, test_sql_from_backup.capitalize())
         assert os.path.isfile(test_back_file)
 
         # run backup
-        sql.drop_table(ms_schema, test_sql_from_backup)
+        sql.drop_table(ms_schema, test_sql_from_backup.capitalize())
         sql.create_table_from_backup(test_back_file)
 
         # validate table exists
         assert sql.table_exists(test_sql_from_backup, schema=ms_schema)
-        sql.query(f"select count(*) cnt from {ms_schema}.{test_sql_from_backup}")
+        sql.query(f"select count(*) cnt from {ms_schema}.{test_sql_from_backup.capitalize()}")
         assert sql.data[0][0] == 10
 
         # Validate schema matches
@@ -575,12 +575,12 @@ class TestBackupTablesMs:
         assert not os.path.isfile(test_back_file)
 
     def test_backup_tables_capitalized(self):
-        sql.drop_table(table=test_sql_to_backup.capitalize(), schema=ms_schema)
+        sql.drop_table(table=test_sql_to_backup, schema=ms_schema)
 
         # table schema
-        sql.drop_table(ms_schema, test_sql_to_backup.capitalize())
+        sql.drop_table(ms_schema, test_sql_to_backup)
         sql.query(f"""
-            CREATE TABLE {ms_schema}.{test_sql_to_backup.capitalize()} (
+            CREATE TABLE {ms_schema}.{test_sql_to_backup} (
                 id int,
                 column2 text,
                 column3 datetime,
@@ -590,20 +590,20 @@ class TestBackupTablesMs:
         # populate table
         for i in range(10):
             sql.query(f"""
-                INSERT INTO {ms_schema}.{test_sql_to_backup.capitalize()}
+                INSERT INTO {ms_schema}.{test_sql_to_backup}
                     (id, column2, column3, "1 test messy column")
                 values ({i}, '{i}', '{'2022-10-14 10:12:40'}', '{'test '*i}')
             """)
         # validate table created
-        assert sql.table_exists(test_sql_to_backup.capitalize(), schema=ms_schema)
-        sql.query(f"select count(*) cnt from {ms_schema}.{test_sql_to_backup.capitalize()}")
+        assert sql.table_exists(test_sql_to_backup, schema=ms_schema)
+        sql.query(f"select count(*) cnt from {ms_schema}.{test_sql_to_backup}")
         assert sql.data[0][0] == 10
 
         if os.path.isfile(test_back_file):
             os.remove(test_back_file)
         assert not os.path.isfile(test_back_file)
 
-        sql.backup_table(ms_schema, test_sql_to_backup.capitalize(), test_back_file, ms_schema, test_sql_from_backup.capitalize())
+        sql.backup_table(ms_schema, test_sql_to_backup, test_back_file, ms_schema, test_sql_from_backup.capitalize())
         assert os.path.isfile(test_back_file)
 
         # run backup
@@ -612,17 +612,17 @@ class TestBackupTablesMs:
 
         # validate table exists
         assert sql.table_exists(test_sql_from_backup.capitalize(), schema=ms_schema)
-        sql.query(f"select count(*) cnt from {ms_schema}.{test_sql_from_backup}")
+        sql.query(f"select count(*) cnt from {ms_schema}.{test_sql_from_backup.capitalize()}")
         assert sql.data[0][0] == 10
 
         # Validate schema matches
-        _to = sql.get_table_columns(test_sql_to_backup.capitalize(), schema=ms_schema)
+        _to = sql.get_table_columns(test_sql_to_backup, schema=ms_schema)
         _from = sql.get_table_columns(test_sql_from_backup.capitalize(), schema=ms_schema)
         assert _to == _from
 
         # clean up
         sql.cleanup_new_tables()
-        assert not sql.table_exists(test_sql_to_backup.capitalize(), schema=ms_schema)
+        assert not sql.table_exists(test_sql_to_backup, schema=ms_schema)
         assert not sql.table_exists(test_sql_from_backup.capitalize(), schema=ms_schema)
         if os.path.isfile(test_back_file):
             os.remove(test_back_file)
