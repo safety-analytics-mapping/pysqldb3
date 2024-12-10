8c5fa81af1a3ba0f408cd58e364fdbe81c55459c
Cindy Chen <cchen1@dot.nyc.gov>
2024-12-10 11:43:45 -0500

    Replace .format with f strings

--- pysqldb3/tests/test_drop_table.py
+++ pysqldb3/tests/test_drop_table.py
@@ -39,27 +39,27 @@ class TestDropTablePG:
         assert not db.table_exists(test_for_drop_table, schema=schema)
 
     def test_remove_table_from_log(self):
-        query_string = """
+        query_string = f"""
 
-        DROP TABLE IF EXISTS working.{};
-        CREATE TABLE working.{} AS 
+        DROP TABLE IF EXISTS working.{test_for_drop_table};
+        CREATE TABLE working.{test_for_drop_table} AS 
 
         SELECT 'a' as col1, 'b' as col2;
 
-        INSERT INTO working.__temp_log_table_{}__ (tbl_id, table_owner,table_schema,table_name,created_on,expires) VALUES (0, '{}', 'working','{}','09/18/2020',null);
+        INSERT INTO working.__temp_log_table_{db.user}__ (tbl_id, table_owner,table_schema,table_name,created_on,expires) VALUES (0, '{db.user}', 'working','{test_for_drop_table}','09/18/2020',null);
 
-        """.format(test_for_drop_table, test_for_drop_table, db.user, db.user, test_for_drop_table)
+        """
 
         db.query(query_string)
         db.drop_table('working', test_for_drop_table)
 
-        log_check_query_string = """
+        log_check_query_string = f"""
         
             SELECT tbl_id, table_owner, table_schema, table_name, created_on, expires
-            FROM working.__temp_log_table_{}__
-            WHERE table_name = '{}'
+            FROM working.__temp_log_table_{db.user}__
+            WHERE table_name = '{test_for_drop_table}'
             
-        """.format(db.user, test_for_drop_table)
+        """
 
         assert len(db.dfquery(log_check_query_string)) == 0
 
@@ -113,15 +113,15 @@ class TestDropTableMS:
         table_name = 'sql_test_table_{}'.format(sql.user)  # from helper.py
 
         schema = 'dbo'
-        query_string = """
+        query_string = f"""
 
-        IF OBJECT_ID('dbo.{0}', 'u') IS NOT NULL
-        DROP TABLE dbo.{0};
+        IF OBJECT_ID('dbo.{test_for_drop_table}', 'u') IS NOT NULL
+        DROP TABLE dbo.{test_for_drop_table};
 
         SELECT 'a' as col1, 'b' as col2
-        INTO dbo.{0};
+        INTO dbo.{test_for_drop_table};
 
-        """.format(test_for_drop_table)
+        """
 
         sql.query(query_string)
         sql.drop_table(schema, table=test_for_drop_table)
@@ -129,72 +129,71 @@ class TestDropTableMS:
         assert not sql.table_exists(test_for_drop_table, schema=schema)
 
     def test_remove_table_from_log(self):
-        table_name = 'sql_test_table_{}'.format(sql.user)  # from helper.py
-        query_string = """
+        table_name = f'sql_test_table_{sql.user}'  # from helper.py
+        query_string = f"""
 
-        IF OBJECT_ID('dbo.{dt}', 'u') IS NOT NULL
-        DROP TABLE dbo.{dt};
+        IF OBJECT_ID(dbo.{test_for_drop_table}, 'u') IS NOT NULL
+        DROP TABLE dbo.{test_for_drop_table};
 
         SELECT 'a' as col1, 'b' as col2
-        INTO dbo.{dt};
+        INTO dbo.{test_for_drop_table};
 
-        INSERT INTO [dbo].[__temp_log_table_{u}__] (table_owner,table_schema,table_name,created_on,expires) 
-        VALUES ('{u}', 'dbo','{dt}','09/18/2020',null);
+        INSERT INTO [dbo].[__temp_log_table_{sql.user}__] (table_owner,table_schema,table_name,created_on,expires) 
+        VALUES ('{sql.user}', 'dbo','{test_for_drop_table}','09/18/2020',null);
 
-        """.format(dt=test_for_drop_table, u=sql.user)
+        """
 
         sql.query(query_string)
         sql.drop_table('dbo', table=test_for_drop_table)
 
-        log_check_query_string = """
+        log_check_query_string = f"""
             SELECT tbl_id, table_owner, table_schema, table_name, created_on, expires
-            FROM [dbo].[__temp_log_table_{}__]
-            WHERE table_name = '{}'
-        """.format(sql.user, test_for_drop_table)
+            FROM [dbo].[__temp_log_table_{sql.user}__]
+            WHERE table_name = '{test_for_drop_table}'
+        """
 
         assert len(sql.dfquery(log_check_query_string)) == 0
 
     def test_table_not_in_log(self):
-        table_name = 'sql_test_table_{}'.format(sql.user)  # from helper.py
-        query_string = """
+        table_name = f'sql_test_table_{sql.user}'  # from helper.py
+        query_string = f"""
 
-        IF OBJECT_ID('dbo.{t}', 'u') IS NOT NULL
-        DROP TABLE dbo.{t};
+        IF OBJECT_ID('dbo.{test_for_drop_table}', 'u') IS NOT NULL
+        DROP TABLE dbo.{test_for_drop_table};
 
 
         SELECT 'a' as col1, 'b' as col2
-        INTO dbo.{t};
+        INTO dbo.{test_for_drop_table};
 
-        """.format(t=test_for_drop_table)
+        """
 
         sql.query(query_string)
         sql.drop_table('dbo', table=test_for_drop_table)
 
-        log_check_query_string = """
+        log_check_query_string = f"""
             SELECT tbl_id, table_owner, table_schema, table_name, created_on, expires
-            FROM [dbo].[__temp_log_table_{}__]
-            WHERE table_name = '{}'
-        """.format(sql.user, test_for_drop_table)
+            FROM [dbo].[__temp_log_table_{sql.user}__]
+            WHERE table_name = '{test_for_drop_table}'
+        """
 
         assert len(sql.dfquery(log_check_query_string)) == 0
 
     def test_table_DNE_but_in_log(self):
-        query_string = """
+        query_string = f"""
 
-        INSERT INTO [dbo].[__temp_log_table_{}__] (table_owner,table_schema,table_name,created_on,expires) 
-        VALUES ('{}', 'dbo','{}','09/18/2020',null);
+        INSERT INTO [dbo].[__temp_log_table_{sql.user}__] (table_owner,table_schema,table_name,created_on,expires) 
+        VALUES ('{sql.user}', 'dbo','{test_for_drop_table}','09/18/2020',null);
 
-        """.format(sql.user, sql.user, test_for_drop_table)
+        """
 
         sql.query(query_string)
         sql.drop_table('dbo', table=test_for_drop_table)
 
-        log_check_query_string = """
+        log_check_query_string = f"""
             SELECT tbl_id, table_owner, table_schema, table_name, created_on, expires
-            FROM [dbo].[__temp_log_table_{}__]
-            WHERE table_name = '{}'
-        """.format(sql.user, test_for_drop_table)
-
+            FROM [dbo].[__temp_log_table_{sql.user}__]
+            WHERE table_name = '{test_for_drop_table}'
+        """
         assert len(sql.dfquery(log_check_query_string)) == 0
 
     def test_table_does_not_exist(self):
